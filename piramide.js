4f957b21b613f7b2843b3b761be4ce6da6228e46
Muriel Santos <muriel.sena@gmail.com>
2017-06-18 02:04:41 -0300

    identação

--- piramide.js
+++ piramide.js
@@ -1,25 +1,25 @@
-   var inputNumero = document.getElementById("alturaDaPiramide");
+var inputNumero = document.getElementById("alturaDaPiramide");
 
-    function criarPiramide(){
-        var altura = Number(inputNumero.value); //Comentário 1.
-        var aux = 1; // incremento relativo à altura da piramide e ao incremento. 
+function criarPiramide(){
+  var altura = Number(inputNumero.value); //Comentário 1.
+  var aux = 1; // incremento relativo à altura da piramide e ao incremento. 
                    
-        for(var x = 1; x <= altura; x++){        	
-	   		if(x == 1){	   			
-		   		document.write("_".repeat(Math.max(0, altura - x))); //Comentário 2.
-		        document.write("#");
-		        document.write("<br>");   			
-	    	}else{	    		
-		    	document.write("_".repeat(Math.max(0, altura - x)));
-		        document.write("#".repeat(x + aux));		        
-		        document.write("<br>");
-		        aux++;		    
-	    	}	   		   			      	
-        }
+  for(var x = 1; x <= altura; x++){        	
+	  if(x == 1){	   			
+		  document.write("_".repeat(Math.max(0, altura - x))); //Comentário 2.
+		  document.write("#");
+		  document.write("<br>");   			
+	  }else{	    		
+		  document.write("_".repeat(Math.max(0, altura - x)));
+		  document.write("#".repeat(x + aux));		        
+		  document.write("<br>");
+		  aux++;		    
+	    }	   		   			      	
     }
-    var btCriar = document.getElementById("btCriarPiramide");
-    btCriar.onclick = criarPiramide;
-  
+  }
+
+var btCriar = document.getElementById("btCriarPiramide");
+btCriar.onclick = criarPiramide;  
 
   //Comentário 1. A função Number(); = Retorna o valor dado pelo usuário como um numeral e não um texto.
 
